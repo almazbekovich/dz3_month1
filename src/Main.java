eaa164c3b901bd3d86d79bcb1f4a4d363e637481
Almazbekovich <bekzatjakshybayev983@gmail.com>
2023-10-07 14:51:08 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {//сигнатура метода
+        for (int i = 1; i <= 4 ; i= i+1) {
+            System.out.println("hello" +i);
+
+        }
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println( "5*" + i + "=" + 5*i);
+        }
+        System.out.println("___________________________________________________");
+        int a =0;
+        while (a<5){
+            System.out.println("Java"+ a);
+            a++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
